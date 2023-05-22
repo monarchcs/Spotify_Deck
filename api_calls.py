7db3324ff488de1eb0a9633f8f6e20418f4e887e
monarchcs <63817601+monarchcs@users.noreply.github.com>
2023-05-22 13:51:55 -0600

    Base test file

---
+++ api_calls.py
@@ -0,0 +1,125 @@
+import os
+import sys
+import json
+import webbrowser
+import spotipy
+import spotipy.util as util
+from json.decoder import JSONDecodeError
+
+#for convenience
+#set SPOTIPY_CLIENT_ID='f677b01ff18f46f9a9e19044b00af5ad'
+#set SPOTIPY_CLIENT_SECRET='057c14386cd541e984af51f55c6b9ab3'
+#set  SPOTIPY_REDIRECT_URI='http://google.com/callback/'
+#py api_calls.py 56n6jy66fjt1c199jq2fl5x44
+
+
+# Get the username from terminal
+username = sys.argv[1]
+scope = 'user-read-private user-read-playback-state user-modify-playback-state' 
+#https://open.spotify.com/user/56n6jy66fjt1c199jq2fl5x44?si=cd8bdfdf88c54ca2
+#user id = '56n6jy66fjt1c199jq2fl5x44'
+client_id = 'f677b01ff18f46f9a9e19044b00af5ad'
+client_secrect = '057c14386cd541e984af51f55c6b9ab3'
+redirect_uri = 'http://google.com/callback/'
+#Erase cache and prompt for user permission
+
+
+
+try:
+    token = util.prompt_for_user_token(username, scope, client_id, client_secrect, redirect_uri)
+except:
+    os.remove(f".cache-{username}")
+    token = util.prompt_for_user_token(username, scope, client_id, client_secrect, redirect_uri)
+#create our spotifyObject
+spotifyObject = spotipy.Spotify(auth=token)
+user = spotifyObject.current_user()
+#print(json.dumps(user, sort_keys=True, indent=4))
+
+#get current device
+devices = spotifyObject.devices()
+print(json.dumps(devices, sort_keys=True, indent=4))
+deviceID = devices['devices'][0]['id']
+
+#current playing track
+track = spotifyObject.current_user_playing_track()
+print(json.dumps(track, sort_keys=True, indent=4))
+print()
+#artist = track['item']['artists'][0]['name']
+#track = track['items']['name']
+
+#if artist != "":
+#    print("Currently playing " + artist + " - " + track)
+
+
+displayName = user['display_name']
+followers = user['followers']['total']
+
+while True:
+    print()
+    print(">>> Welcome to Spotipy " + displayName + "!")
+    print(">>> You have " + str(followers) + " followers.")
+    print()
+    print("0 - search for an artist")
+    print("1 - exit")
+    print()
+    choice = input("your choice: ")
+
+    if choice == "0":
+        print()
+        searchQuery = input("Ok, whats their name: ")
+        print()
+        
+        #search results
+        searchResults = spotifyObject.search(searchQuery,1,0,"artist")
+        #print(json.dumps(searchResults, sort_keys=True, indent=4))
+
+        #artist details
+        artist = searchResults['artists']['items'][0]
+        print(artist['name'])
+        print(str(artist['followers']['total']) + " followers")
+        print(artist['genres'][0])
+        print()
+        webbrowser.open(artist['images'][0]['url'])
+        artistID = artist['id']
+        #album and track details
+        trackURIs = []
+        trackArt = []
+        z = 0
+
+        #extract album data
+        albumResults = spotifyObject.artist_albums(artistID)
+        albumResults = albumResults['items']
+
+        for item in albumResults:
+            print("Album " + item['name'])
+            albumID = item['id']
+            albumArt = item['images'][0]['url']
+
+            #extract track data
+            trackResults = spotifyObject.album_tracks(albumID)
+            trackResults = trackResults['items']
+
+            for item in trackResults:
+                print(str(z) + ": " + item['name'])
+                trackURIs.append(item['uri'])
+                trackArt.append(albumArt)
+                z+=1
+        print()
+
+        # see album art
+
+        while True:
+            songSelection = input("Enter a song number to see the album art(x to exit): ")
+            if songSelection == "x":
+                break
+            webbrowser.open(trackArt[int(songSelection)])
+            trackSelectionList = []
+            trackSelectionList.append(trackURIs[int(songSelection)])
+            spotifyObject.start_playback(deviceID, None, trackSelectionList)
+            webbrowser.open(trackArt[int(songSelection)])
+            
+                
+    if choice == "1":
+        break
+
+#print(json.dumps(VARIABLE, sort_keys=True, indent=4))
